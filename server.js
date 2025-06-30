ca6518dc6b6ee47d6ac887edf96bcdb12056eb04
edwinkesuma <53287515+edwinkesuma@users.noreply.github.com>
2025-07-01 00:06:16 +0700

    connecting database with the app

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
-const dotenv = require('dotenv');
-dotenv.config({ path: './config.env' });
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config({ path: "./config.env" });
 
-const app = require('./app');
+const app = require("./app");
+
+const DB = process.env.DATABASE.replace("<PASSWORD>", process.env.DATABASE_PASSWORD);
+
+mongoose.connect(DB, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => {
+  console.log("DB Connected!");
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
